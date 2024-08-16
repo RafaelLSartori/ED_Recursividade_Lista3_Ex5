6cc9dea54cc891fe46e000bb5c04222a16da8bf3
Rafael Sartori <160548884+RafaelLSartori@users.noreply.github.com>
2024-08-16 12:18:49 -0300

    Atualização

--- src/controller/Ex5.java
+++ src/controller/Ex5.java
@@ -1,2 +1,16 @@
-package controller;public class Ex5 {
+package controller;
+
+public class Ex5 {
+    public Ex5(){
+        super();
+    }
+    public int frecfib(int n){
+        //O ponto de parada se dá sabendo que a 1ª e 2ª posição da sequência de fibonacci é sempre 1.
+        if( n == 1 || n == 2 ){
+            return 1;
+        }else{
+            //Eu chamei a recursiva fazendo a soma dos dois termos anteriores.
+            return frecfib(n - 1 ) + frecfib( n - 2 );
+        }
+    }
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,5 +1,17 @@
+package view;
+
+import controller.Ex5;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Ex5 m = new Ex5();
+        int num = Integer.parseInt(JOptionPane.showInputDialog("Digite uma posição da série: "));
+        while (num > 20){
+            JOptionPane.showMessageDialog(null,"O número da posição precisa ser menor que 20.");
+            num = Integer.parseInt(JOptionPane.showInputDialog("Digite novamente uma posição da série: "));
+        }
+        System.out.println(m.frecfib(num));
     }
 }
